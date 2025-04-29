98c0fc58cd382cef1396bca74a7f0c72f134db9a
Jonathan Ato Markin <jona10711@gmail.com>
2025-04-29 23:26:43 +0200

    Project setup

---
+++ src/main/java/com/markin/tomcat_minidash/TomcatMinidashApplication.java
@@ -0,0 +1,13 @@
+package com.markin.tomcat_minidash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatMinidashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatMinidashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markin/tomcat_minidash/TomcatMinidashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markin.tomcat_minidash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatMinidashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
